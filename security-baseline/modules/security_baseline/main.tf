b782c5c42fcdeaeed947820406825fc07871155d
Raja Sekhar Y <115916860+rajasekhary23@users.noreply.github.com>
2025-06-17 21:38:57 +0530

    Create main.tf

---
+++ security-baseline/modules/security_baseline/main.tf
@@ -0,0 +1,48 @@
+provider "aws" {
+  region = var.region
+}
+
+# CloudTrail
+resource "aws_cloudtrail" "main" {
+  name                          = "org-trail"
+  s3_bucket_name                = var.cloudtrail_s3_bucket
+  include_global_service_events = true
+  is_multi_region_trail         = true
+  enable_log_file_validation    = true
+  is_logging                    = true
+}
+
+# AWS Config
+resource "aws_config_configuration_recorder" "main" {
+  name     = "default"
+  role_arn = var.config_role_arn
+  recording_group {
+    all_supported = true
+    include_global_resource_types = true
+  }
+}
+
+resource "aws_config_delivery_channel" "main" {
+  name           = "default"
+  s3_bucket_name = var.config_s3_bucket
+  depends_on     = [aws_config_configuration_recorder.main]
+}
+
+resource "aws_config_configuration_recorder_status" "main" {
+  name     = aws_config_configuration_recorder.main.name
+  is_enabled = true
+  depends_on = [aws_config_delivery_channel.main]
+}
+
+# GuardDuty
+resource "aws_guardduty_detector" "main" {
+  enable = true
+}
+
+# Security Hub
+resource "aws_securityhub_account" "main" {}
+
+resource "aws_securityhub_standards_subscription" "cis" {
+  standards_arn = "arn:aws:securityhub:::standards/cis-aws-foundations-benchmark/v/1.2.0"
+  depends_on    = [aws_securityhub_account.main]
+}
